31eff247a976ae307798ced18205a17d12c06f81
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2024-06-17 17:10:36 +0100

    some service and launcher code

--- OpenKAC_Launcher/main.cpp
+++ OpenKAC_Launcher/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <filesystem>
 #include <set>
+#include <Windows.h>
 #include <psapi.h>
 #include <tchar.h>
+
 #include <argh.h>
 
 #include "def.hpp"
@@ -53,7 +55,7 @@ int BlackListedDriver() {
 		return ret;
 	}
 	// quick check before loading driver and you could let the user know of said driver running.
-	std::set<std::string> drvs = { "KProcessHacker3" };
+	std::set<std::string> drvs = { "KProcessHacker3","KProcessHacker2" };
 	for (DWORD i = 0; i < numServices; ++i)
 	{
 #ifdef LIST_RUNNING_DRIVERS
@@ -78,10 +80,9 @@ int main(int argc, char** argv) {
 	argh::parser cmdl(argc,argv);
 
 	if (cmdl[{"-p", "--process"}]) {
-
+		//TODO
 	}
 
-	//std::filsy
 	auto bldRes = BlackListedDriver();
 	switch (bldRes)
 	{
@@ -95,16 +96,34 @@ int main(int argc, char** argv) {
 		std::cout << "Checking for backlisted driver failed: " << std::hex << bldRes << std::dec << std::endl;
 		break;
 	}
-	//TODO call execute OpenKAC_Service.
 
 	auto driver = CreateFile(L"\\\\.\\OpenKAC",GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0);
-	if (driver == INVALID_HANDLE_VALUE) {
+	if (driver != INVALID_HANDLE_VALUE) {
+		std::cout << "Driver already running please close the game that is using it." << std::endl;
 		std::cin.get();
-		exit(0);
+		//exit(0);
 	}
+
+	//TODO call execute OpenKAC_Service.
+	STARTUPINFOA si = {0};
+	PROCESS_INFORMATION pi = {0};
+	si.cb = sizeof(si);
+	//TODO check if file exists
+	CreateProcessA((std::string(DRIVER_FILE_LOC) + "OpenKAC_Service.exe").c_str(), 0, 0, 0, 0, CREATE_NEW_PROCESS_GROUP, 0,0,&si,&pi);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	driver = CreateFile(L"\\\\.\\OpenKAC", GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+	if (driver == INVALID_HANDLE_VALUE) {
+		std::cout << "Driver not running." << std::endl;
+		std::cin.get();
+		//exit(0);
+	}
+
 	HANDLE proc = (HANDLE)GetCurrentProcessId();
 	std::cout << "sending proc: " << std::hex << proc << std::dec << std::endl;
 	SendProcID(driver, proc);
+	std::cin.get();
 	CloseHandle(driver);
 	return 0;
 }
--- OpenKAC_Service/main.cpp
+++ OpenKAC_Service/main.cpp
@@ -4,17 +4,38 @@
 #include <process.h>
 #include <iostream>
 #include <filesystem>
-
+#include <tchar.h>
+#include <strsafe.h>
+SERVICE_STATUS svcStatus;
+SERVICE_STATUS_HANDLE svcStatusHandle;
+HANDLE svsEvent = 0 ;
 #include "def.hpp"
+VOID ReportSvcStatus(DWORD dwCurrentState,
+	DWORD dwWin32ExitCode,
+	DWORD dwWaitHint);
 
-DWORD ServiceHandlerProc(DWORD dwControl,DWORD dwEventType,LPVOID lpEventData,LPVOID lpContext)
+void WINAPI ServiceHandlerProc(DWORD dwControl)
 {
-	UNREFERENCED_PARAMETER(dwControl);
-	UNREFERENCED_PARAMETER(dwEventType);
-	UNREFERENCED_PARAMETER(lpEventData);
-	UNREFERENCED_PARAMETER(lpContext);
+	switch (dwControl)
+	{
+	case SERVICE_CONTROL_STOP:
+		ReportSvcStatus(SERVICE_STOP_PENDING, NO_ERROR, 0);
 
-	return 0;
+		// Signal the service to stop.
+
+		SetEvent(svsEvent);
+		ReportSvcStatus(svcStatus.dwCurrentState, NO_ERROR, 0);
+
+		return; 
+
+	case SERVICE_CONTROL_INTERROGATE:
+		break;
+
+	default:
+		break;
+	}
+
+	return;
 }
 
 bool CreateKACService() {
@@ -28,9 +49,11 @@ bool CreateKACService() {
 			);
 			if (svs) {
 				//TODO:
+				std::cout << "Created service" << std::endl;
+				return true;
 			}
 			else {
-				std::cout << "Failed to create service";
+				std::cout << "Failed to create service" << std::endl;
 				CloseServiceHandle(scm);
 			}
 
@@ -59,40 +82,163 @@ BYTE ServiceExists() {
 	else
 		return KAC_SCMANGER_FAIL;
 }
+
+VOID ReportSvcStatus(DWORD dwCurrentState,
+	DWORD dwWin32ExitCode,
+	DWORD dwWaitHint)
+{
+	static DWORD dwCheckPoint = 1;
+
+	// Fill in the SERVICE_STATUS structure.
+
+	svcStatus.dwCurrentState = dwCurrentState;
+	svcStatus.dwWin32ExitCode = dwWin32ExitCode;
+	svcStatus.dwWaitHint = dwWaitHint;
+
+	if (dwCurrentState == SERVICE_START_PENDING)
+		svcStatus.dwControlsAccepted = 0;
+	else svcStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+
+	if ((dwCurrentState == SERVICE_RUNNING) ||
+		(dwCurrentState == SERVICE_STOPPED))
+		svcStatus.dwCheckPoint = 0;
+	else svcStatus.dwCheckPoint = dwCheckPoint++;
+
+	// Report the status of the service to the SCM.
+	SetServiceStatus(svcStatusHandle, &svcStatus);
+}
+
 void ServiceProc(DWORD dwNumServicesArgs, LPSTR* lpServiceArgVectors) {
 
 	UNREFERENCED_PARAMETER(dwNumServicesArgs);
 	UNREFERENCED_PARAMETER(lpServiceArgVectors);
 
-	SERVICE_STATUS_HANDLE status = RegisterServiceCtrlHandlerExA(KAC_SERVICE_PROC_NAME,&ServiceHandlerProc,0);
-	if (status != 0) {
+	svcStatusHandle = RegisterServiceCtrlHandlerA(KAC_SERVICE_NAME,ServiceHandlerProc);
+	if (svcStatusHandle != 0) {
+		svcStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+		svcStatus.dwServiceSpecificExitCode = 0;
+	}
 
+	ReportSvcStatus(SERVICE_START_PENDING, NO_ERROR, 3000);
+
+	svsEvent = CreateEventA(
+		NULL,    // default security attributes
+		TRUE,    // manual reset event
+		FALSE,   // not signaled
+		NULL);   // no name
+	if (svsEvent == 0) {
+		std::cout << "Can not create service event" << std::endl;
+		ReportSvcStatus(SERVICE_STOPPED, GetLastError(), 0);
+		return;
 	}
-	if (ServiceExists() == FALSE) {
-		CreateKACService();
-	}
-	else {
-		// why are we here, we shouldn't be. 
+	ReportSvcStatus(SERVICE_RUNNING, NO_ERROR, 0);
+
+	while (1)
+	{
 	
+		WaitForSingleObject(svsEvent, INFINITE);
+
+		ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+
+		return;
+
 	}
 }
 
+VOID SvsReportEvent(LPTSTR szFunction)
+{
+	HANDLE hEventSource;
+	LPCSTR lpszStrings[2];
+	CHAR Buffer[80];
 
+	hEventSource = RegisterEventSourceA(NULL, KAC_SERVICE_NAME);
 
+	if (NULL != hEventSource)
+	{
+		StringCchPrintfA(Buffer, 80,"%s failed with %d", szFunction, GetLastError());
+
+		lpszStrings[0] = KAC_SERVICE_NAME;
+		lpszStrings[1] = Buffer;
+
+		ReportEventA(hEventSource,        // event log handle
+			EVENTLOG_ERROR_TYPE, // event type
+			0,                   // event category
+			1,					 // event identifier
+			NULL,                // no security identifier
+			2,                   // size of lpszStrings array
+			0,                   // no binary data
+			lpszStrings,         // array of strings
+			NULL);               // no binary data
+
+		DeregisterEventSource(hEventSource);
+	}
+}
+//https://stackoverflow.com/questions/55906598/iswindows10orgreater-always-returns-false-even-with-manifest-file
+//normal IsWindows10OrGreater always returned false even when on windows 10.
+inline bool isWindows10OrGreater()
+{
+	
+	INT32(NTAPI * RtlGetVersion)(PRTL_OSVERSIONINFOW lpVersionInformation) = nullptr;
+	HMODULE ntdll = GetModuleHandle(L"ntdll.dll");
+	if (ntdll == NULL)
+		return false;
+	*reinterpret_cast<FARPROC*>(&RtlGetVersion) = GetProcAddress(ntdll, "RtlGetVersion");
+	if (RtlGetVersion == nullptr)
+		return false;
+	OSVERSIONINFOEX versionInfo{ sizeof(OSVERSIONINFOEX) };
+	if (RtlGetVersion(reinterpret_cast<LPOSVERSIONINFO>(&versionInfo)) < 0)
+		return false;
+
+	if (versionInfo.dwMajorVersion > HIBYTE(_WIN32_WINNT_WIN10))
+		return true;
+
+	if (versionInfo.dwMajorVersion == HIBYTE(_WIN32_WINNT_WIN10))
+	{
+		if (versionInfo.dwMinorVersion > LOBYTE(_WIN32_WINNT_WIN10))
+			return true;
+
+		if (versionInfo.dwMinorVersion == LOBYTE(_WIN32_WINNT_WIN10) && versionInfo.dwBuildNumber >= 19041)
+			return true;
+	}
+
+	return false;
+}
 
 
 int main(int argc, char** argv) {
 
-	if (!IsWindows10OrGreater()) {
+	if (!isWindows10OrGreater()) {
 		MessageBoxA(0, "Only supports Windows 10 or greater", "Error", MB_OK);
 		return 0;
+	}	
+	if (ServiceExists() == FALSE) {
+		CreateKACService();
 	}
-	SERVICE_TABLE_ENTRYA ste;
-	ste.lpServiceName = KAC_SERVICE_PROC_NAME;
-	ste.lpServiceProc = &ServiceProc;
-	if (StartServiceCtrlDispatcherA(&ste)) {
-		//TODO: error handling
-		return 0;
+
+	auto scm = OpenSCManagerA(0, 0, SC_MANAGER_ALL_ACCESS);
+	if (scm) {
+		auto svs = OpenServiceA(scm, KAC_SERVICE_NAME, SC_MANAGER_ALL_ACCESS);
+		if (svs == 0 && GetLastError() == ERROR_SERVICE_DOES_NOT_EXIST) {
+			std::cout << "Can't get OpenKAC service. It doesn't exsist" << std::endl;
+			return ERROR_SERVICE_DOES_NOT_EXIST;
+		}
+		else if (svs == 0)
+			std::cout << "Can't get OpenKAC service" << std::endl;
+		std::cout << "Starting Service" << std::endl;
+		if (StartServiceA(svs, 0, 0) == 0) {
+			auto err = GetLastError();
+			std::cout << "StartService failed: " << err << std::endl;
+			return err;
+		}
+	}
+	else
+		std::cout << "Can't open services manager" << std::endl;
+	SERVICE_TABLE_ENTRYA ste[] = { {KAC_SERVICE_NAME, (LPSERVICE_MAIN_FUNCTIONA)ServiceProc},{0,0} };
+	if (StartServiceCtrlDispatcherA(ste)) {
+		auto err = GetLastError();
+		std::cout << "StartServiceCtrlDispatcher error: "<< err << std::endl;
+
+		return err;
 	}
 
 }
